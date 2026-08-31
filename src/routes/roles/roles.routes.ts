03b4406e8bb5490ca6b62e3b4069ae735444d15c
Sparsh Nigam <sparshnigam945@gmail.com>
2026-08-31 21:22:24 +0530

    roles api wip

--- src/routes/roles/roles.routes.ts
+++ src/routes/roles/roles.routes.ts
@@ -1,4 +1,4 @@
-import { rolesController } from "@/controllers/roles.controller.js";
+import { rolesController } from "@/controllers/user/roles.controller.js";
 import { authorize } from "@/middlewares/auth.js";
 import { Router } from "express";
 
